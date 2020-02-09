b8fd4733d20654d9d006cc4cadffb9bfd360840e
Nikola Rados <nrados@uvic.ca>
2020-02-08 23:00:58 -0800

    Format using python black

--- bot.py
+++ bot.py
@@ -4,14 +4,22 @@ from argparse import ArgumentParser
 
 # Get token from command line for now
 parser = ArgumentParser()
-parser.add_argument('--token', '-t', action='store', required=True, help='Token from the developer portal')
+parser.add_argument(
+    "--token",
+    "-t",
+    action="store",
+    required=True,
+    help="Token from the developer portal",
+)
 args = parser.parse_args()
 
 # Simple bot setup
-client = commands.Bot(command_prefix='!')
+client = commands.Bot(command_prefix="!")
+
 
 @client.event
 async def on_ready():
-    print('Bot is ready')
+    print("Bot is ready")
 
-client.run(f'{args.token}')
+
+client.run(f"{args.token}")
