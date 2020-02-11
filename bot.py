0c8d25733ff1a20f1ade53708e6b26b755506c98
Nikola Rados <nrados@uvic.ca>
2020-02-10 17:01:55 -0800

    Add kick/ban commands

--- bot.py
+++ bot.py
@@ -24,22 +24,32 @@ async def on_ready():
 
 @client.event
 async def on_member_join(member):
-    print(f'{member} has joined the server')
+    print(f"{member} has joined the server")
 
 
 @client.event
 async def on_member_remove(member):
-    print(f'{member} has left the server')
+    print(f"{member} has left the server")
 
 
 @client.command()
 async def ping(ctx):
-    await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
+    await ctx.send(f"Pong! {round(client.latency * 1000)}ms")
 
 
 @client.command()
 async def clear(ctx, amount=5):
     await ctx.channel.purge(limit=amount)
+
+
+@client.command()
+async def kick(ctx, member: discord.Member, *, reason=None):
+    await member.kick(reason=reason)
+
+
+@client.command()
+async def ban(ctx, member: discord.Member, *, reason=None):
+    await member.ban(reason=reason)
 
 
 client.run(f"{args.token}")
