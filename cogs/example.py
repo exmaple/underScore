852d8a92fc960da6817d988a59d662ab649675e3
Nikola Rados <nrados@uvic.ca>
2020-02-11 18:06:26 -0800

    Add some basic error handling

--- cogs/example.py
+++ cogs/example.py
@@ -28,13 +28,18 @@ class Example(commands.Cog):
     async def on_member_remove(self, member):
         print(f"{member} has left the server")
 
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        if isinstance(error, commands.CommandNotFound):
+            await ctx.send("Invalid command")
+
     # Commands
     @commands.command()
     async def ping(self, ctx):
         await ctx.send(f"Pong! {round(self.client.latency * 1000)}ms")
 
     @commands.command()
-    async def clear(self, ctx, amount=5):
+    async def clear(self, ctx, amount: int):
         await ctx.channel.purge(limit=amount)
 
     @commands.command()
@@ -44,6 +49,12 @@ class Example(commands.Cog):
     @commands.command()
     async def ban(self, ctx, member: discord.Member, *, reason=None):
         await member.ban(reason=reason)
+
+    # Error handlers
+    @clear.error
+    async def clear_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send("Please specifiy the number of messages to delete.")
 
 
 def setup(client):
