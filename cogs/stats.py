e80aba122eba4cf65fd5f98cbfd3c1739773c8b7
Nikola Rados <nrados@uvic.ca>
2020-02-17 17:55:56 -0800

    Add dummy command

--- cogs/stats.py
+++ cogs/stats.py
@@ -1,5 +1,8 @@
 import discord
 from discord.ext import commands
+from random import choice, randrange
+
+from utils.messages import embedded_player_stats
 
 
 class Stats(commands.Cog):
@@ -7,8 +10,19 @@ class Stats(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def player_stats(self, ctx, player_name):
-        await ctx.send(f"{player_name} stats")
+    async def stats(self, ctx, player_name):
+        info = {
+            "height": randrange(150, 200),
+            "weight": randrange(50, 110),
+            "position": choice(["GK", "CB", "CM", "ST"]),
+        }
+        stats = {
+            "gp": randrange(1, 50),
+            "goals": randrange(50),
+            "assists": randrange(50),
+        }
+        embed = embedded_player_stats(player_name, info, stats)
+        await ctx.send(embed=embed)
 
 
 def setup(bot):
