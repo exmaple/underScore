ab966259b77986d29d669c8f8487d34022cd8037
Nikola Rados <nrados@uvic.ca>
2020-02-18 21:43:57 -0800

    Use table building functions

--- cogs/stats.py
+++ cogs/stats.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 from random import choice, randrange
 
-from utils.messages import embedded_player_stats
+from utils.messages import embedded_player_stats, embedded_team_stats
 
 
 class Stats(commands.Cog):
@@ -10,7 +10,7 @@ class Stats(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def stats(self, ctx, player_name):
+    async def player(self, ctx, player_name):
         info = {
             "height": randrange(150, 200),
             "weight": randrange(50, 110),
@@ -23,6 +23,22 @@ class Stats(commands.Cog):
         }
         embed = embedded_player_stats(player_name, info, stats)
         await ctx.send(embed=embed)
+
+    @commands.command()
+    async def team(self, ctx, team_name):
+        stats = {
+            "pos": '1',
+            "gp": '22',
+            "w": '14',
+            "t": '4',
+            'l': '4',
+            'gf': '62',
+            'ga': '24',
+            'gd': '38',
+            'p': '46'
+        }
+        embed = embedded_team_stats(team_name, stats)
+        await ctx.send(embed)
 
 
 def setup(bot):
--- utils/messages.py
+++ utils/messages.py
@@ -1,4 +1,5 @@
 import discord
+from utils.tables import create_table
 
 
 def embedded_player_stats(player_name, info, stats):
@@ -12,3 +13,16 @@ def embedded_player_stats(player_name, info, stats):
     embed.add_field(name="Goals", value=stats["goals"], inline=True)
     embed.add_field(name="Assists", value=stats["assists"], inline=True)
     return embed
+
+def embedded_team_stats(team_name, stats):
+    """Build embedded message with team statistics"""
+    return create_table(team_name, stats)
+    # embed = discord.Embed(title=team_name, color=0xDC052D)
+    # embed.set_thumbnail(url="https://clipartart.com/images/bavaria-logo-clipart.jpg")
+    # embed.add_field(name="", value=info["height"], inline=True)
+    # embed.add_field(name="Weight (kg)", value=info["weight"], inline=True)
+    # embed.add_field(name="Position", value=info["position"], inline=True)
+    # embed.add_field(name="Games Played", value=stats["gp"], inline=True)
+    # embed.add_field(name="Goals", value=stats["goals"], inline=True)
+    # embed.add_field(name="Assists", value=stats["assists"], inline=True)
+    # return embed
