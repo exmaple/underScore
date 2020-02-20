cec84cefa2f94e64f8cabbd91bcb0f3055ced38d
Nikola Rados <nrados@uvic.ca>
2020-02-19 22:39:46 -0800

    Add simple tests for util methods

---
+++ tests/test_dummy_data.py
@@ -0,0 +1,12 @@
+from utils.dummy_data import get_dummy_data
+
+def test_get_dummy_data():
+    result_count = 0
+    for result in get_dummy_data():
+        result_count += 1
+        if isinstance(result, int):
+            assert result > 0
+        elif isinstance(result, str):
+            assert len(result) == 2
+            
+    assert result_count == 6
