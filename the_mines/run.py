df8d3b7c351a2ab682e7c299acd1db26c1b76574
Max Kasprzik <m.kasprzik04@gmail.com>
2020-02-22 09:18:50 -0800

    formatted with black

--- the_mines/run.py
+++ the_mines/run.py
@@ -3,4 +3,4 @@ from scrape_html import fussballdatenpunktde_matchday_results
 
 html_file = get_html()
 
-fussballdatenpunktde_matchday_results('data/'+html_file)
+fussballdatenpunktde_matchday_results("data/" + html_file)
--- the_mines/scrape_html.py
+++ the_mines/scrape_html.py
@@ -2,23 +2,24 @@ from bs4 import BeautifulSoup
 from argparse import ArgumentParser
 import unicodedata
 
+
 def fussballdatenpunktde_matchday_results(site_html):
-    '''
+    """
     fussballdaten.de
     view-source:https://www.fussballdaten.de/bundesliga/2020/12/
-    '''
+    """
     # print(site_html)
     with open(f"{site_html}") as fp:
-        soup = BeautifulSoup(fp, 'html.parser')
+        soup = BeautifulSoup(fp, "html.parser")
 
     game_count = 0
     game_dict = {}
-    a_tag = soup('a')
+    a_tag = soup("a")
     for game_details in a_tag:
 
-        if 'id' in game_details.attrs and 'class' in game_details.attrs:
+        if "id" in game_details.attrs and "class" in game_details.attrs:
             teams_list = []
-            teams_string = game_details.get('title')
+            teams_string = game_details.get("title")
             teams_string_list = teams_string.split()
             counter = 0
             score1 = -1
@@ -29,14 +30,14 @@ def fussballdatenpunktde_matchday_results(site_html):
                 scores = str(scores.text)
                 score1 = scores[0]
                 score2 = scores[2]
-                counter +=1
-            if teams_string_list[2] == 'gegen' and len(teams_string_list) == 6:
+                counter += 1
+            if teams_string_list[2] == "gegen" and len(teams_string_list) == 6:
                 # 1/1
                 team1 = umlaut(teams_string_list[1])
                 team2 = umlaut(teams_string_list[3])
                 teams_list.append((team1, score1))
                 teams_list.append((team2, score2))
-            elif teams_string_list[3] == 'gegen' and len(teams_string_list) == 7:
+            elif teams_string_list[3] == "gegen" and len(teams_string_list) == 7:
                 # 2/1
                 team1 = teams_string_list[1] + " " + teams_string_list[2]
                 team2 = teams_string_list[4]
@@ -44,7 +45,7 @@ def fussballdatenpunktde_matchday_results(site_html):
                 team2 = umlaut(team2)
                 teams_list.append((team1, score1))
                 teams_list.append((team2, score2))
-            elif teams_string_list[2] == 'gegen' and len(teams_string_list) == 7:
+            elif teams_string_list[2] == "gegen" and len(teams_string_list) == 7:
                 # 1/2
                 team1 = teams_string_list[1]
                 team2 = teams_string_list[3] + " " + teams_string_list[4]
@@ -52,7 +53,7 @@ def fussballdatenpunktde_matchday_results(site_html):
                 team2 = umlaut(team2)
                 teams_list.append((team1, score1))
                 teams_list.append((team2, score2))
-            elif teams_string_list[3] == 'gegen' and len(teams_string_list) == 8:
+            elif teams_string_list[3] == "gegen" and len(teams_string_list) == 8:
                 # 2/2
                 team1 = teams_string_list[1] + " " + teams_string_list[2]
                 team2 = teams_string_list[4] + " " + teams_string_list[5]
@@ -66,12 +67,14 @@ def fussballdatenpunktde_matchday_results(site_html):
 
     print(game_dict)
 
+
 def umlaut(word_with_umlaut):
-    if '\\xc3\\xb6' in word_with_umlaut:
-        word_with_umlaut = word_with_umlaut.replace('\\xc3\\xb6', 'ö')
-    elif '\\xc3\\xbc' in word_with_umlaut:
-        word_with_umlaut = word_with_umlaut.replace('\\xc3\\xbc', 'ü')
+    if "\\xc3\\xb6" in word_with_umlaut:
+        word_with_umlaut = word_with_umlaut.replace("\\xc3\\xb6", "ö")
+    elif "\\xc3\\xbc" in word_with_umlaut:
+        word_with_umlaut = word_with_umlaut.replace("\\xc3\\xbc", "ü")
     return word_with_umlaut
+
 
 def main():
     parser = ArgumentParser()
@@ -86,5 +89,5 @@ def main():
     fussballdatenpunktde_matchday_results(args)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
