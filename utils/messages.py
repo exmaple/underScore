702dde422ef381edaa1ef0ca259d587ebbbe18e2
Max Kasprzik <m.kasprzik04@gmail.com>
2020-02-27 07:22:12 -0800

    added function for matchday results message

--- utils/messages.py
+++ utils/messages.py
@@ -18,3 +18,13 @@ def embedded_stats(title, **stats):
         embed.add_field(name=name, value=stat, inline=True)
 
     return embed
+
+def embedded_matchday_results():
+    """Build embedded message with matchday results"""
+    embed = discord.Embed(title=title, color=0xDC052D)
+    embed.set_thumbnail(url="https://clipartart.com/images/bavaria-logo-clipart.jpg")
+
+    for name, stat in stats.items():
+        embed.add_field(name=name, value=stat, inline=True)
+
+    return embed
