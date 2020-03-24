5b5595d973ea1ef5677189fa88605f2ae03a9740
Max Kasprzik <m.kasprzik04@gmail.com>
2020-03-24 08:55:07 -0700

    removing setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-__version__ = (0, 3, 1)
-
-setup(
-    name = 'underScore',
-    packages = find_packages(),
-    # package_data = {'data': ['heroes.json', 'raw_data.json']}
-)
