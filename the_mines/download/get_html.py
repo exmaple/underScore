7e6d214b4362d334337bf49cfba202340f913d99
Nikola Rados <nrados@uvic.ca>
2020-05-25 17:18:17 -0700

    Simplify data download process

---
+++ the_mines/download/get_html.py
@@ -0,0 +1,29 @@
+import requests
+import shutil
+import os
+from tempfile import TemporaryFile
+
+
+def download_html(matchday, season):
+    """Download fussballdaten html file
+
+    The URL of this website uses only the latter year in a season (2019/2020).
+    `season[5:9]` selects the latter season. I'm using the 2019/2020 convention
+    for season input because I believe it is what most users are familiar with
+    when asked for a season.
+
+    Args:
+        matchday (str): desired matchday
+        season (str): desired season
+
+    Returns:
+        data str containing downloaded html page content
+    """
+    try:
+        url = "https://www.fussballdaten.de/bundesliga/" + season[5:9] + "/" + matchday
+        data = str(requests.get(url))
+
+    except Exception:
+        print(f'Unable to download file using params: {matchday}, {season}')
+
+    return data
