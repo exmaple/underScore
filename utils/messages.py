a3d095e01edc262ce92da2d9da0ebde343101dd0
Nikola Rados <nrados@uvic.ca>
2020-06-06 12:00:23 -0700

    Build embed with dict

--- utils/messages.py
+++ utils/messages.py
@@ -2,7 +2,7 @@ import discord
 
 
 def embedded_stats(title, **stats):
-    """Build embedded message with player statistics
+    """Build embedded message with generic statistics
 
     Args:
         title (str): title of the embedded message
@@ -18,6 +18,17 @@ def embedded_stats(title, **stats):
         embed.add_field(name=name, value=stat, inline=True)
 
     return embed
+
+
+def embed_dict(title, stats):
+    embed = discord.Embed(title=title, color=0xDC052D)
+    embed.set_thumbnail(url="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRWeGvePKW6vZ6kM5Wn6ZFqprk7lzoi73qd_MMlnoF46uIARNRA&usqp=CAU")
+
+    for name, stat in stats.items():
+        embed.add_field(name=name, value=stat, inline=False)
+
+    return embed
+
 
 
 def embedded_matchday_results(matchday, matches):
