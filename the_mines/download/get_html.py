94374d072f35390225f1b0ca5c9973222753f869
Nikola Rados <nrados@uvic.ca>
2020-06-06 12:01:56 -0700

    Generalize the download function

--- the_mines/download/get_html.py
+++ the_mines/download/get_html.py
@@ -5,24 +5,17 @@ import logging
 logger = logging.getLogger("app")
 
 
-def download_html(matchday, season):
-    """Download fussballdaten html file
-
-    The URL of this website uses only the latter year in a season (2019/2020).
-    `season[5:9]` selects the latter season. I'm using the 2019/2020 convention
-    for season input because I believe it is what most users are familiar with
-    when asked for a season.
+def download_raw_html(url):
+    """Download html page data
 
     Args:
-        matchday (str): desired matchday
-        season (str): desired season
+        url (str): target url to download
 
     Returns:
-        data str containing downloaded html page content
+        raw html string output
     """
     try:
         logger.debug("Downloading data file")
-        url = "https://www.fussballdaten.de/bundesliga/" + season[5:9] + "/" + matchday
         data = str(requests.get(url).content)
 
     except Exception:
