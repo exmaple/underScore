de214c6b2d20489068269d5d866d1bb078895e31
Max Kasprzik <m.kasprzik04@gmail.com>
2020-06-16 18:09:19 -0700

    removed unused function

--- the_mines/download/get_html.py
+++ the_mines/download/get_html.py
@@ -20,29 +20,3 @@ def download_raw_html(url):
         logger.debug(f"Unable to download file using params: {matchday}, {season}")
 
     return data
-
-
-def download_html(matchday, season):
-    """Download fussballdaten html file
-
-    The URL of this website uses only the latter year in a season (2019/2020).
-    `season[5:9]` selects the latter season. I'm using the 2019/2020 convention
-    for season input because I believe it is what most users are familiar with
-    when asked for a season.
-
-    Args:
-        matchday (str): desired matchday
-        season (str): desired season
-
-    Returns:
-        data str containing downloaded html page content
-    """
-    try:
-        logger.debug("Downloading data file")
-        url = "https://www.fussballdaten.de/bundesliga/" + season[5:9] + "/" + matchday
-        data = str(requests.get(url).content)
-
-    except Exception:
-        logger.debug(f"Unable to download file using params: {matchday}, {season}")
-
-    return data
