091c68a6f029c2ab552e7c7a953391dda9d711a6
nikola-rados <nrados@uvic.ca>
2020-06-25 23:14:23 -0700

    Format with black

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -1,56 +1,62 @@
 import pytest
 import re
-from utils.misc import umlaut, unumlaut, format_date, open_default_html, get_default_matchday, get_default_season
+from utils.misc import (
+    umlaut,
+    unumlaut,
+    format_date,
+    open_default_html,
+    get_default_matchday,
+    get_default_season,
+)
 
 
-@pytest.mark.parametrize(("word", "expected"), [
-    ("f\\xc3\\xb6\\xc3\\xb6", "föö"),
-    ("bar", "bar"),
-    ("f\\xc3\\xbctbol", "fütbol"),
-])
+@pytest.mark.parametrize(
+    ("word", "expected"),
+    [("f\\xc3\\xb6\\xc3\\xb6", "föö"), ("bar", "bar"), ("f\\xc3\\xbctbol", "fütbol"),],
+)
 def test_umlaut(word, expected):
     assert umlaut(word) == expected
 
 
-@pytest.mark.parametrize(("word", "expected"), [
-    ("föö", "foo"),
-    ("bär", "bar"),
-    ("fütbol", "futbol"),
-    ("áēïōü", "aeiou"),
-    ("test", "test")
-])
+@pytest.mark.parametrize(
+    ("word", "expected"),
+    [
+        ("föö", "foo"),
+        ("bär", "bar"),
+        ("fütbol", "futbol"),
+        ("áēïōü", "aeiou"),
+        ("test", "test"),
+    ],
+)
 def test_unumlaut(word, expected):
     assert unumlaut(word) == expected
 
 
-@pytest.mark.parametrize(("date", "expected"), [
-    ("10.10.2010", "October 10, 2010"),
-    ("1.2.2012", "February 1, 2012"),
-])
+@pytest.mark.parametrize(
+    ("date", "expected"),
+    [("10.10.2010", "October 10, 2010"), ("1.2.2012", "February 1, 2012"),],
+)
 def test_format_date(date, expected):
     assert format_date(date) == expected
 
 
 @pytest.mark.online
-@pytest.mark.parametrize(("pattern"), [
-    ("Die aktuelle Bundesliga 20\d\d/20\d\d - Der \d*\. Spieltag - Fussballdaten")
-])
+@pytest.mark.parametrize(
+    ("pattern"),
+    [("Die aktuelle Bundesliga 20\d\d/20\d\d - Der \d*\. Spieltag - Fussballdaten")],
+)
 def test_open_default_html(pattern):
     assert re.match(pattern, open_default_html())
 
 
 @pytest.mark.online
-@pytest.mark.parametrize(("earliest", "latest"), [
-    (1, 34)
-])
+@pytest.mark.parametrize(("earliest", "latest"), [(1, 34)])
 def test_get_default_matchday(earliest, latest):
     result = int(get_default_matchday())
-    assert (result >= earliest and result <= latest)
+    assert result >= earliest and result <= latest
 
 
 @pytest.mark.online
-@pytest.mark.parametrize(("pattern"), [
-    ("20\d\d")
-])
+@pytest.mark.parametrize(("pattern"), [("20\d\d")])
 def test_get_default_season(pattern):
     assert re.match(pattern, get_default_season())
--- tests/test_table_handler.py
+++ tests/test_table_handler.py
@@ -2,7 +2,12 @@ import pytest
 from tempfile import TemporaryFile
 from bs4 import BeautifulSoup
 
-from utils.table_handler import tables_from_soup, get_table, find_team_in_table, extract_full_table_stats
+from utils.table_handler import (
+    tables_from_soup,
+    get_table,
+    find_team_in_table,
+    extract_full_table_stats,
+)
 from the_mines.download.get_html import download_raw_html
 
 
@@ -14,35 +19,52 @@ def get_soup(url="https://www.fussballdaten.de/bundesliga/tabelle/2019"):
 
 
 @pytest.mark.online
-@pytest.mark.parametrize("expected", [
-    (4)
-])
+@pytest.mark.parametrize("expected", [(4)])
 def test_tables_from_soup(expected):
     assert len(tables_from_soup(get_soup())) == expected
 
 
 @pytest.mark.online
-@pytest.mark.parametrize(("full", "form", "home", "away", "expected"), [
-    (True, False, False, False, 19),
-    (False, True, False, False, 4),
-    (False, False, True, False, 4),
-    (False, False, False, True, 4),
-])
+@pytest.mark.parametrize(
+    ("full", "form", "home", "away", "expected"),
+    [
+        (True, False, False, False, 19),
+        (False, True, False, False, 4),
+        (False, False, True, False, 4),
+        (False, False, False, True, 4),
+    ],
+)
 def test_get_table(full, form, home, away, expected):
-    assert len(get_table(tables_from_soup(get_soup()), full=full, form=form, home=home, away=away)) == expected
+    assert (
+        len(
+            get_table(
+                tables_from_soup(get_soup()), full=full, form=form, home=home, away=away
+            )
+        )
+        == expected
+    )
 
 
 @pytest.mark.online
-@pytest.mark.parametrize(("team", "table", "expected"), [
-    ("bayern", get_table(tables_from_soup(get_soup()), full=True), 10)
-])
+@pytest.mark.parametrize(
+    ("team", "table", "expected"),
+    [("bayern", get_table(tables_from_soup(get_soup()), full=True), 10)],
+)
 def test_find_team_in_table(team, table, expected):
     assert len(find_team_in_table(team, table)) == expected
 
 
 @pytest.mark.online
-@pytest.mark.parametrize(("row", "expected"), [
-    (find_team_in_table("bayern", get_table(tables_from_soup(get_soup()), full=True)), ("1", "Bayern", "34", "24", "6", "4", "88:32", "56", "78"))
-])
+@pytest.mark.parametrize(
+    ("row", "expected"),
+    [
+        (
+            find_team_in_table(
+                "bayern", get_table(tables_from_soup(get_soup()), full=True)
+            ),
+            ("1", "Bayern", "34", "24", "6", "4", "88:32", "56", "78"),
+        )
+    ],
+)
 def test_extract_full_table_stats(row, expected):
     assert extract_full_table_stats(row) == expected
