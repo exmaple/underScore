3c806d7f2ac2af2d734be9b9c598dd3dd9e69a23
Nikola Rados <nrados@uvic.ca>
2020-06-27 10:46:24 -0700

    Format with black

--- tests/test_process_matchday.py
+++ tests/test_process_matchday.py
@@ -11,9 +11,26 @@ from the_mines.process.fussballdaten.process_matchday import (
 
 
 @pytest.mark.online
-@pytest.mark.parametrize(("matchday", "season", "expected"), [
-    ("34", "2020", [[('Frankfurt', '3'), ('Paderborn', '2')], [('Bremen', '6'), ('Köln', '1')], [('Freiburg', '4'), ('Schalke', '0')], [('Augsburg', '1'), ('RB Leipzig', '2')], [('Union Berlin', '3'), ('Düsseldorf', '0')], [('Dortmund', '0'), ('Hoffenheim', '4')], [('Leverkusen', '1'), ('Mainz', '0')], [("M\\'gladbach", '2'), ('Hertha BSC', '1')], [('Wolfsburg', '0'), ('Bayern', '4')]])
-])
+@pytest.mark.parametrize(
+    ("matchday", "season", "expected"),
+    [
+        (
+            "34",
+            "2020",
+            [
+                [("Frankfurt", "3"), ("Paderborn", "2")],
+                [("Bremen", "6"), ("Köln", "1")],
+                [("Freiburg", "4"), ("Schalke", "0")],
+                [("Augsburg", "1"), ("RB Leipzig", "2")],
+                [("Union Berlin", "3"), ("Düsseldorf", "0")],
+                [("Dortmund", "0"), ("Hoffenheim", "4")],
+                [("Leverkusen", "1"), ("Mainz", "0")],
+                [("M\\'gladbach", "2"), ("Hertha BSC", "1")],
+                [("Wolfsburg", "0"), ("Bayern", "4")],
+            ],
+        )
+    ],
+)
 def test_process_results(matchday, season, expected):
     results = process_results(matchday, season)["27.06.2020"]
     for match in expected:
