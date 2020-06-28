e97b534b070716f3ade47230707b4709c52aa99d
Max Kasprzik <m.kasprzik04@gmail.com>
2020-06-28 07:17:00 -0700

    add custom help command helper functions

--- utils/messages.py
+++ utils/messages.py
@@ -1,5 +1,37 @@
 import discord
 
+def help_title_card():
+    embed = discord.Embed(title='Help Info', color=0xffff72)
+
+    embed.add_field(
+        name='How to use commands:',
+        value='![command] [option1 option2 ...]',
+        inline=False
+    )
+
+    return embed
+
+def embedded_help_command(command):
+    embed = discord.Embed(title='`'+command.name+'`', color=0xffff72)
+    args = '> '
+    try:
+        for arg_tuple in command.usage:
+            if command.usage.index(arg_tuple) == len(command.usage)-1:
+                args += arg_tuple[0] +': '+ arg_tuple[1] +'\n'
+            else:
+                args += arg_tuple[0] +': '+ arg_tuple[1] +'\n > '
+    except TypeError:
+        args='No Args'
+
+    embed.add_field(
+        # name=f"{command}\n",
+        name='__Options:__',
+        value=args,
+        inline=False
+    )
+
+    return embed
+
 
 def embedded_stats(title, **stats):
     """Build embedded message with generic statistics
